a588fd36d928ea967d70fd3b22464a397b9d67e0
Noah Solomon <noahsolomon2003@gmail.com>
2024-03-21 14:45:48 -0400

    toaster and navbar

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,17 @@
+import { Button } from "./ui/button";
+
+export default function Navbar() {
+  return (
+    <div className=" absolute top-8 left-0 right-0 border rounded-lg shadow-sm p-2 flex justify-between w-[800px] max-w-[90%] items-center mx-auto bg-card">
+      <img
+        className="w-[70px] h-[70px] rounded-full border"
+        alt="donoharm"
+        src="/donoharm.png"
+      />
+      <div className="flex flex-row gap-2">
+        <Button variant={"outline"}>Log in</Button>
+        <Button>Join us</Button>
+      </div>
+    </div>
+  );
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
+import { Toaster } from "./components/ui/sonner.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
+    <Toaster richColors position="top-center" />
   </React.StrictMode>,
 );
