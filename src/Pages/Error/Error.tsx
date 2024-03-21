dc7782263bacff952778cdae370571ab904732e9
Noah Solomon <noahsolomon2003@gmail.com>
2024-03-21 15:10:23 -0400

    center smh

--- src/Pages/Error/Error.tsx
+++ src/Pages/Error/Error.tsx
@@ -7,7 +7,9 @@ export default function Error() {
       <div className="flex h-full flex-col items-center justify-center">
         {/* <p className="text-2xl font-krypton">DO NO HARM </p> */}
         <div className="flex flex-col gap-4 items-center justify-center">
-          <p className="text-3xl font-krypton">YOU SEEM LOST, FRIEND</p>
+          <p className="text-center text-3xl font-krypton">
+            YOU SEEM LOST, FRIEND
+          </p>
           <Link className={buttonVariants()} to={"/"}>
             Return Home
           </Link>
