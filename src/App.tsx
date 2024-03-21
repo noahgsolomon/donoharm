934e73d07b60dfc395f6c99b4bfb958d388c7117
Noah Solomon <noahsolomon2003@gmail.com>
2024-03-21 13:55:36 -0400

    initial commit :)

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./globals.css";
+import { Button } from "./components/ui/button";
+import { Toaster } from "./components/ui/sonner";
+
+function App() {
+  return (
+    <>
+      <Button>sup</Button>
+      <Toaster richColors position="top-center" />
+    </>
+  );
+}
+
+export default App;
