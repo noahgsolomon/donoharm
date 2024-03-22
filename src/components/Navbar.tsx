30f532de9bbb811ece59df84f9d46ceb9dc9940d
Noah Solomon <noahsolomon2003@gmail.com>
2024-03-21 21:03:25 -0400

    link to home

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,13 +1,20 @@
+import { Link } from "react-router-dom";
 import { Button } from "./ui/button";
 
 export default function Navbar() {
   return (
     <div className=" absolute top-8 left-0 right-0 border rounded-lg shadow-sm p-2 flex justify-between w-[800px] max-w-[90%] items-center mx-auto bg-card">
-      <img
-        className="w-[50px] h-[50px] rounded-full border"
-        alt="donoharm"
-        src="/donoharm.webp"
-      />
+      <Link
+        to={"/"}
+        className="cursor-pointer hover:scale-[101%] active:scale-[99%] transition-all"
+      >
+        <img
+          className=" w-[50px] h-[50px] rounded-full border"
+          alt="donoharm"
+          src="/donoharm.webp"
+        />
+      </Link>
+
       <div className="flex flex-row gap-2">
         <Button variant={"outline"}>Log in</Button>
         <Button>Join us</Button>
