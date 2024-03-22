c4abad54cf1dadaba03beb35d1f245c54e0b2934
Noah Solomon <noahsolomon2003@gmail.com>
2024-03-21 21:04:24 -0400

    home

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -2,20 +2,21 @@ import Navbar from "@/components/Navbar";
 
 export default function Home() {
   return (
-    <div className="h-screen w-screen">
+    <>
       <Navbar />
-      <div className="pt-24 flex h-full flex-col items-center justify-center">
-        {/* <p className="text-2xl font-krypton">DO NO HARM </p> */}
-        <div className="flex flex-col gap-4 items-center justify-center">
-          <div className="font-krypton flex flex-col items-center">
-            <p className="text-3xl">DO NO HARM</p>
-            <span className="flex flex-row text-red-500 items-center justify-center">
-              {"\n"}EVER
-            </span>
-          </div>
+      <div className="h-screen w-screen">
+        <div className="pt-24 flex h-full flex-col items-center justify-center">
+          {/* <p className="text-2xl font-krypton">DO NO HARM </p> */}
+          <div className="flex flex-col gap-4 items-center justify-center">
+            <div className="font-krypton flex flex-col items-center">
+              <p className="text-3xl">DO NO HARM</p>
+              <span className="flex flex-row text-red-500 items-center justify-center">
+                {"\n"}EVER
+              </span>
+            </div>
 
-          <pre className="text-[6px]">
-            {`
+            <pre className="text-[6px]">
+              {`
                                          ***+++======+++***+                                        
                                    +++=:..:--=+++**+++=-:::..-+*-                                   
                               +++=:.-=++*********************+=-::=++*                              
@@ -65,9 +66,10 @@ export default function Home() {
                                       @@@@@@@@@@@@@@@@@@@@@@@@                                      
                                              @@@@@@@@@@                                             
           `}
-          </pre>
+            </pre>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
