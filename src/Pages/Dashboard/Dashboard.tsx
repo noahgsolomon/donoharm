c2a9dea5a239c4fa9e5ded94a219f560e2a57fbb
Noah Solomon <noahsolomon2003@gmail.com>
2024-04-08 20:47:05 -0400

    dashboard implemented with zustand

--- src/Pages/Dashboard/Dashboard.tsx
+++ src/Pages/Dashboard/Dashboard.tsx
@@ -10,39 +10,57 @@ import {
 import { Button } from "@/components/ui/button";
 import { Brain, Plus } from "lucide-react";
 import SharePost from "./components/SharePost";
+import { useState } from "react";
+import useUserStore from "@/UserStore.tsx";
+import Error from "../../Pages/Error/Error.tsx";
+import usePostStore from "@/PostStore.tsx";
 
 export default function Dashboard() {
+  const isLoggedIn = useUserStore((state) => state.isLoggedIn);
+
+  const posts = usePostStore((state) => state.posts);
+
   return (
     <>
       <Navbar />
-      <div className="text-center pt-12 font-krypton">
-        <p>INFLUENCE YOUR MIND</p>
-        <p className="text-red-500 text-xs">PROTECT AT ALL COSTS</p>
+      {isLoggedIn ? (
+        <>
+          <div className="text-center pt-12 font-krypton">
+            <p>INFLUENCE YOUR MIND</p>
+            <p className="text-red-500 text-xs">PROTECT AT ALL COSTS</p>
 
-        <p className="text-xs">
-          share thought {"->"}
-          <SharePost />
-        </p>
-      </div>
-
-      <div className="overflow-x-hidden flex h-[50vh] flex-col items-center justify-center">
-        <Carousel className="flex w-full flex-col items-center justify-center">
-          <CarouselContent>
-            {Array.from({ length: 5 }).map((_, index) => (
-              <CarouselItem
-                className="flex items-center justify-center"
-                key={index}
-              >
-                <PostCard text="In a world where words are weapons, Every utterance becomes a transgression." />
-              </CarouselItem>
-            ))}
-          </CarouselContent>
-          <div className="flex w-full justify-center gap-4 pt-4">
-            <CarouselPrevious />
-            <CarouselNext />
+            <p className="text-xs">
+              share thought {"->"}
+              <SharePost />
+            </p>
           </div>
-        </Carousel>
-      </div>
+
+          <div className="overflow-x-hidden flex h-[50vh] flex-col items-center justify-center">
+            <Carousel className="flex w-full flex-col items-center justify-center">
+              <CarouselContent>
+                {posts.map((post, index) => (
+                  <CarouselItem
+                    className="flex items-center justify-center"
+                    key={index}
+                  >
+                    <PostCard
+                      image={post.image}
+                      name={post.name}
+                      text={post.text}
+                    />
+                  </CarouselItem>
+                ))}
+              </CarouselContent>
+              <div className="flex w-full justify-center gap-4 pt-4">
+                <CarouselPrevious />
+                <CarouselNext />
+              </div>
+            </Carousel>
+          </div>
+        </>
+      ) : (
+        <Error />
+      )}
     </>
   );
 }
